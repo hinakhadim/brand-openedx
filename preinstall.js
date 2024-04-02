3e4aa54ef803a4ec834b7d148cc5f0e228a020bc
hinakhadim <hina.khadim@arbisoft.com>
2024-04-02 15:46:29 +0500

    fix: testnig 6

--- preinstall.js
+++ preinstall.js
@@ -1,5 +1,6 @@
-const fs = require("fs-extra");
-const { execSync } = require('child_process');
+// const fs = require("fs-extra");
+// const { execSync } = require('child_process');
+const fs = require("fs");
 const path = require('path');
 
 const theme = process.env.npm_config_theme;
@@ -11,8 +12,7 @@ switch (theme) {
     case 'light':
       console.log('Installing sub-package-light...');
       srcDir = path.resolve(__dirname, 'light/paragon/_variables.scss');
-      throw 'hina';
-      destDir = path.resolve(process.cwd(), 'node_modules', '@edx/brand/paragon');
+      destDir = path.resolve(__dirname, 'paragon/');
       console.log("----src-path-----", srcDir, destDir)
 
 // Copy the src directory to the destination
@@ -24,15 +24,14 @@ switch (theme) {
       srcDir = path.resolve(__dirname, 'dark/paragon/_variables.scss');
       console.log("-----src path-----", srcDir)
       
-      destDir = path.resolve('node_modules', '@edx/brand/paragon');
+      destDir = path.resolve(__dirname, 'paragon/');
       console.log("-----path-----", srcDir, destDir)
       console.log(__dirname);
         console.log(process.cwd());
         console.log(process.env.PWD);
-      throw 'hinia';
 
 // Copy the src directory to the destination
-        fs.copySync(srcDir, destDir);
+        fs.copyFileSync(srcDir, destDir);
     //   execSync('npm install sub-package-dark', { stdio: 'inherit' });
       break;
     default:
